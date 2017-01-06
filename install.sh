65af7c0e2810e10cf3f8e7b83da34cf81227f568
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2017-01-06 17:45:57 -0500

    check that zsh is installed

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
-# setup oh-my-zsh and customize
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-git clone git://github.com/jocelynmallon/zshmarks.git ~/.oh-my-zsh/custom/plugins/zshmarks
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+#!/usr/bin/env bash
+OMZ=~/.oh-my-zsh
+USR=dumps
+
+hash zsh || { echo "Make sure zsh is installed and in PATH" 1>&2; exit; }
+
+# download oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git $OMZ
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $OMZ/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search.git $OMZ/custom/plugins/zsh-history-substring-search
+
+# deploy confs
+[ -e ~/.zshrc ] && { cp ~/.zshrc ~/.zshrc.bak; }
 cp zshrc ~/.zshrc
-cp dumps.zsh dumps.zsh-theme ~/.oh-my-zsh/custom/
+cp $USR.zsh $USR.zsh-theme $OMZ/custom/.
