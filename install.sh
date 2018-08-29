c9c4a252cea5c35b643ea68b90664f25c68129ee
Thomas Hackl <thackl@lim4.de>
2018-08-29 09:06:17 -0400

    only clone oh-my-zsh etc. if necessary

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 OMZ=~/.oh-my-zsh
+SYN=$OMZ/custom/plugins/zsh-syntax-highlighting
 USR=dumps
 
 hash zsh || { echo "Make sure zsh is installed and in PATH" 1>&2; exit; }
 
 # download oh-my-zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git $OMZ
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $OMZ/custom/plugins/zsh-syntax-highlighting
+if [ ! -d "$OMZ" ]; then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git $OMZ
+fi
+
+if [ ! -d "$SYN" ]; then
+    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $SYN
+fi
 
 # deploy confs
 [ -e ~/.zshrc ] && { cp ~/.zshrc ~/.zshrc.bak; }
